c3d86276a1304362e216f798b3a78a6b313759bd
christykail <christykailfilm@gmail.com>
2022-06-19 18:20:04 +0100

    Like... made it all work

--- report_to_log.py
+++ report_to_log.py
@@ -1,22 +1,50 @@
-
+#!/usr/local/bin/python3
+import re
+import tkinter as tk
 import pyperclip
+
+__version__ = '2.0.0'
+
+
+class ReportToLogApp(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title('Report to Log')
+        self.geometry('500x400')
+
+        # set column and row weights
+        self.columnconfigure(0, weight=1)
+
+        self.button = tk.Button(self, text='Reformat clipboard', command=self.run, width=30, pady=10)
+        self.button.grid(row=0, column=0, sticky='ew', padx=25, pady=25)
+
+        self.label = tk.Label(self, text='Copy the "reels" data from a shooting day \n'
+                                         'report, then click reformat clipboard', font=('Monaco', 14), pady=10)
+        self.label.grid(row=1, column=0, sticky='ew', padx=25, pady=25)
+
+    def run(self):
+
+        result, text = report_to_log()
+
+        if result:
+
+            text = re.sub(r'\t+', '\t', text)
+            self.label.config(text=text)
+            print(text)
 
 
 def report_to_log():
-
-    content = pyperclip.waitForNewPaste()
+    content = pyperclip.paste()
 
     # if clipboard is empty, return
     if content == '':
         print('Clipboard is empty')
-        return False
+        return False, 'Clipboard is empty'
 
     # if clipboard is not empty, split it into lines
     lines = content.split('\r')
-
-    # if len(lines[0].split()) != 8:
-    #     print('Invalid format - 1')
-    #     return False
 
     output_lines = []
 
@@ -24,18 +52,17 @@ def report_to_log():
         row = line.split()
         if len(row) != 8:
             print('Invalid format - 2')
-            return False
+            return False, 'Invalid format'
 
         result = f'{row[0]}\t\t\t\t{row[6]}\t{row[5]}\t{row[2]}\t{format_duration(row[3])}'
         output_lines.append(result)
 
     pyperclip.copy('\n'.join(output_lines))
 
-    return True
+    return True, '\n'.join(output_lines)
 
 
 def format_duration(entry):
-
     hmsf_list = entry.split(":")
 
     while len(hmsf_list) < 3:
@@ -50,10 +77,5 @@ def format_duration(entry):
 
 
 if __name__ == '__main__':
-
-    while True:
-
-        if report_to_log():
-            print(pyperclip.paste())
-
-
+    app = ReportToLogApp()
+    app.mainloop()
